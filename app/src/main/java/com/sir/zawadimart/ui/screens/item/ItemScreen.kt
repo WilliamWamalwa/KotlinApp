2ecfc349262bcd32805078fb1be56a58ee83bb34
vabianchacha <fabianchacha03@gmail.com>
2025-04-03 15:26:26 +0300

    final commit

--- app/src/main/java/com/sir/zawadimart/ui/screens/item/ItemScreen.kt
+++ app/src/main/java/com/sir/zawadimart/ui/screens/item/ItemScreen.kt
@@ -3,22 +3,30 @@ package com.sir.zawadimart.ui.screens.item
 import android.media.Image
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -69,13 +77,28 @@ Column(
     )
 
     //end
-
+    Spacer(modifier = Modifier.height(20.dp))
     Image(
         painter = painterResource(R.drawable.potato),
         contentDescription = "potato",
         modifier = Modifier.fillMaxWidth().height(200.dp),
         contentScale = ContentScale.FillWidth
     )
+    Spacer(modifier = Modifier.height(20.dp))
+
+//searchbar
+var search by remember { mutableStateOf("") }
+    OutlinedTextField(
+        value = search,
+        onValueChange = { search= it },
+        modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
+        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = ("search")) }
+
+    )
+
+//end of searchbar
+
+
 }
 
 
