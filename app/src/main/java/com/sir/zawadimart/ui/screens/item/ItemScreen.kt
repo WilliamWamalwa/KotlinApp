2de422de22e7f8abe7543eeefe75d8bc0ee71997
vabianchacha <fabianchacha03@gmail.com>
2025-04-03 16:19:35 +0300

    final commit

--- app/src/main/java/com/sir/zawadimart/ui/screens/item/ItemScreen.kt
+++ app/src/main/java/com/sir/zawadimart/ui/screens/item/ItemScreen.kt
@@ -3,18 +3,24 @@ package com.sir.zawadimart.ui.screens.item
 import android.media.Image
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -32,10 +38,14 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sir.zawadimart.R
 import com.sir.zawadimart.ui.theme.coffee
+import com.sir.zawadimart.ui.theme.grey
 import com.sir.zawadimart.ui.theme.titlewhite
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -92,12 +102,218 @@ var search by remember { mutableStateOf("") }
         value = search,
         onValueChange = { search= it },
         modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
-        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = ("search")) }
+        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = ("search")) },
+        placeholder = { Text(text = "Search here")}
 
     )
 
 //end of searchbar
+    Spacer(modifier = Modifier.height(20.dp))
 
+    //row
+    Row (){
+        Image(
+            painter = painterResource(R.drawable.potato),
+            contentDescription = "resturant",
+            modifier = Modifier.width(150.dp).height(150.dp).clip(shape = RoundedCornerShape(20.dp)),
+            contentScale = ContentScale.FillWidth,
+
+        )
+
+        Spacer(modifier = Modifier.width(20.dp))
+
+        Column {
+            Text(
+                text = "Glens Resturant",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold
+
+
+            )
+            Text(
+                text = "Glens Resturant",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold
+
+
+            )
+            Text(
+                    text = "Glens Resturant",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.ExtraBold
+
+
+            )
+            Row {
+            Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                tint = coffee
+            )
+            Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+            Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+            Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+            Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+             }
+
+            Button(
+                onClick ={},
+                colors = ButtonDefaults.buttonColors(coffee),
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.fillMaxWidth().padding( end = 20.dp)
+            ) {
+                Text(text = "Go to Review")
+            }
+
+        }
+
+
+    }
+    //end of row
+
+    Spacer(modifier = Modifier.height(20.dp))
+
+    //row
+    Row (){
+        Image(
+            painter = painterResource(R.drawable.resturant),
+            contentDescription = "resturant",
+            modifier = Modifier.width(150.dp).height(150.dp).clip(shape = RoundedCornerShape(20.dp)),
+            contentScale = ContentScale.FillWidth,
+
+            )
+
+        Spacer(modifier = Modifier.width(20.dp))
+
+        Column {
+            Text(
+                text = "Glens Resturant",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold
+
+
+            )
+            Text(
+                text = "Glens Resturant",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold
+
+
+            )
+            Text(
+                text = "Glens Resturant",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold
+
+
+            )
+            Row {
+                Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+                Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+                Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+                Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+                Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+            }
+
+            Button(
+                onClick ={},
+                colors = ButtonDefaults.buttonColors(coffee),
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.fillMaxWidth().padding( end = 20.dp)
+            ) {
+                Text(text = "Go to Review")
+            }
+
+        }
+
+
+    }
+    //end of row
+
+    Spacer(modifier = Modifier.height(20.dp))
+
+    //row
+    Row (){
+        Image(
+            painter = painterResource(R.drawable.resturant),
+            contentDescription = "resturant",
+            modifier = Modifier.width(150.dp).height(150.dp).clip(shape = RoundedCornerShape(20.dp)),
+            contentScale = ContentScale.FillWidth,
+
+            )
+
+        Spacer(modifier = Modifier.width(20.dp))
+
+        Column {
+            Text(
+                text = "Glens Resturant",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold
+
+
+            )
+            Text(
+                text = "Glens Resturant",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold
+
+
+            )
+            Text(
+                text = "Glens Resturant",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold
+
+
+            )
+            Row {
+                Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+                Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+                Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = coffee
+                )
+                Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = grey
+                )
+                Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = grey
+                )
+            }
+
+            Button(
+                onClick ={},
+                colors = ButtonDefaults.buttonColors(coffee),
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.fillMaxWidth().padding( end = 20.dp)
+            ) {
+                Text(text = "Go to Review")
+            }
+
+        }
+
+
+    }
+    //end of row
 
 }
 
